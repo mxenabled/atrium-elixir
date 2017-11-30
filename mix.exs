98bb7a2da796174e271e352d9a95ca865a1e30a5
Michael Ries <michael@riesd.com>
2017-11-30 15:00:00 -0700

    support elixir 1.4

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Atrium.Mixfile do
     [
       app: :atrium,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.4",
       start_permanent: Mix.env == :prod,
       deps: deps()
     ]
